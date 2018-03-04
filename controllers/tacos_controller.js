03f167f89fabdd72c634ec66f0d9d6f9aa2ba6e9
jstudenski <jstudenski@gmail.com>
2018-03-04 11:48:26 -0600

    file set up and error debugging

--- controllers/tacos_controller.js
+++ controllers/tacos_controller.js
@@ -1,1 +1,10 @@
 var express = require("express");
+var burger = require("../models/taco.js");
+
+var router = express.Router();
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
+
