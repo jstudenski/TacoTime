398c458f20a20250f15b0481a82a4c53f99d15a0
jstudenski <jstudenski@gmail.com>
2018-03-04 11:59:30 -0600

    server up and running

--- controllers/tacos_controller.js
+++ controllers/tacos_controller.js
@@ -3,6 +3,17 @@ var burger = require("../models/taco.js");
 
 var router = express.Router();
 
+router.get("/", function(req, res) {
+  taco.all(function(data) {
+    var hbsObject = {
+      tacos: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+
 
 
 // Export routes for server.js to use.
